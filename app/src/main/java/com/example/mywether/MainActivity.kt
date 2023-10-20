fe4c1e551921d6d1995cf8f37a0a7a89c1896190
Kekuz <anton.edlenko@yandex.ru>
2023-10-20 19:41:34 +0300

    Application define current location, currently asking permissions

--- app/src/main/java/com/example/mywether/MainActivity.kt
+++ app/src/main/java/com/example/mywether/MainActivity.kt
@@ -1,28 +1,76 @@
 package com.example.mywether
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.LocaleManagerCompat
 import androidx.core.view.isVisible
 import androidx.lifecycle.ViewModelProvider
 import com.example.mywether.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var viewModel: MainViewModel
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding.recyclerView.adapter = WeatherAdapter(listOf())
 
-        viewModel.resultLive.observe(this){
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
+
+        viewModel.enabledPermissionLive.observe(this) {
+            requestPermission()
+        }
+
+        viewModel.resultLive.observe(this) {
             binding.recyclerView.adapter = it
             binding.loadPb.isVisible = false
         }
 
-        viewModel.getWeatherInfo()
 
     }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ), 101
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 101) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                viewModel.getLastKnownLocation()
+            } else {
+                Toast.makeText(this, "Denied", Toast.LENGTH_LONG).show()
+                binding.loadPb.isVisible = false
+            }
+        }
+    }
+
 }
--- app/src/main/java/com/example/mywether/MainViewModel.kt
+++ app/src/main/java/com/example/mywether/MainViewModel.kt
@@ -1,38 +1,88 @@
 package com.example.mywether
 
 
+import android.Manifest
+import android.app.Application
+import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.util.Log
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import java.lang.Exception
+import java.util.Locale
 
 
-class MainViewModel : ViewModel() {
+class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     val resultLive = MutableLiveData<WeatherAdapter>()
+    val enabledPermissionLive = MutableLiveData<Boolean>()
+    private val fusedLocationProviderClient =
+        LocationServices.getFusedLocationProviderClient(application)
 
-    fun getWeatherInfo(){
+    init {
+        getLastKnownLocation()
+    }
+    private fun getWeatherInfo(city:String) {
         CoroutineScope(Dispatchers.IO).launch {
             try {
                 val currentWeatherResponse = RetrofitBuilder.weatherService.getWeather(
                     RetrofitBuilder.apiKey,
-                    "Rostov-on-Don",
+                    city,
                     "3",
                     "no",
                     "no",
                 )
-                if (currentWeatherResponse.isSuccessful){
+                if (currentWeatherResponse.isSuccessful) {
                     Log.e("MyLog", currentWeatherResponse.body()?.forecast?.forecastday.toString())
                     resultLive.postValue(WeatherAdapter(currentWeatherResponse.body()?.forecast?.forecastday!!))
                 }
-            }catch (e: Exception){
+            } catch (e: Exception) {
                 Log.e("MyLog", e.message.toString())
             }
 
         }
+    }
+
+    private fun getCityName(lat: Double, long: Double): String {
+        var cityName: String?
+        val geoCoder = Geocoder(getApplication(), Locale.ENGLISH)
+        val address = geoCoder.getFromLocation(lat, long, 1)
+        cityName = address?.get(0)?.adminArea
+        if (cityName == null) {
+            cityName = address?.get(0)?.locality
+            if (cityName == null) {
+                cityName = address?.get(0)?.subAdminArea
+            }
+        }
+        return cityName!!
+    }
+
+    fun getLastKnownLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                getApplication(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                getApplication(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            enabledPermissionLive.value = true
+            return
+        }
+        fusedLocationProviderClient.lastLocation
+            .addOnSuccessListener {
+                if (it != null) {
+                    //cityLive.value = getCityName(it.latitude, it.longitude)
+                    Log.e("location", "${it.latitude} ${it.longitude}")
+                    getWeatherInfo(getCityName(it.latitude, it.longitude))
+                }
+
+            }
     }
 
 
